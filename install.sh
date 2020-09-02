4456f8df877bd49d4f35c16157e326ed55ca6e7b
Alex Kosh <sasha23969@gmail.com>
2020-09-02 21:09:03 +0300

    feat: Added install.sh script

---
+++ install.sh
@@ -0,0 +1,29 @@
+echo "Installing dotfiles..."
+
+DIR=$PWD
+
+read -p "Replace ~/.bashrc? " -n 1 -r; echo
+if [[ $REPLY =~ ^[Yy]$ ]];
+then
+    ln -sf $DIR/.bashrc ~/.bashrc
+fi
+
+read -p "Replace ~/.vimrc? " -n 1 -r; echo
+if [[ $REPLY =~ ^[Yy]$ ]];
+then
+    ln -sf $DIR/.vimrc ~/.vimrc
+fi
+
+read -p "Replace ~/.gitconfig? " -n 1 -r; echo
+if [[ $REPLY =~ ^[Yy]$ ]];
+then
+    ln -sf $DIR/.gitconfig ~/.gitconfig
+fi
+
+read -p "Replace ~/.tmux.conf? " -n 1 -r; echo
+if [[ $REPLY =~ ^[Yy]$ ]];
+then
+    ln -sf $DIR/.tmux.conf ~/.tmux.conf
+fi
+
+echo "Installation complete! Relogin please"
